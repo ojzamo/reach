7ca3a79e9f8a897843c281c168e30902d2e5224e
Oliver Zamora <ojzamo@gmail.com>
2022-08-12 14:03:35 -0600

    session6

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
-import { loadStdlib } from '@reach-sh/stdlib';
-import * as backend from './build/index.main.mjs';
+import { loadStdlib } from "@reach-sh/stdlib";
+import * as backend from "./build/index.main.mjs";
 const stdlib = loadStdlib();
 
 const startingBalance = stdlib.parseCurrency(100);
@@ -11,39 +11,53 @@ const getBalance = async (who) => fmt(await stdlib.balanceOf(who));
 const beforeAlice = await getBalance(accAlice);
 const beforeBob = await getBalance(accBob);
 
-
 const ctcAlice = accAlice.contract(backend);
 const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
-const HAND = ['Rock', 'Paper', 'Scissors'];
-const OUTCOME = ['Bob win', 'Draw', 'Alice win'];
+const HAND = ["Rock", "Paper", "Scissors"];
+const OUTCOME = ["Bob win", "Draw", "Alice win"];
 
 const Player = (Who) => ({
-    ...stdlib.hasRandom,
-    getHand: () => {
-        const hand = Math.floor(Math.random() * 3);
-        console.log(`${Who} played ${HAND[hand]}`);
-        return hand;
-    },
-    seeOutcome: (outcome => {
-        console.log(`${Who} saw outcome ${OUTCOME[outcome]}`)
-    }),
-    informTimeout: () => {
-        console.log(`${Who} observed a timeout`)
+  ...stdlib.hasRandom,
+  getHand: async () => {
+    const hand = Math.floor(Math.random() * 3);
+    console.log(`${Who} played ${HAND[hand]}`);
+    if (Math.random() <= 0.01) {
+      for (let i = 0; i < 10; i++) {
+        console.log(`${Who} takes their sweet time sending it back`);
+        await stdlib.wait(1);
+      }
     }
-})
+
+    return hand;
+  },
+  seeOutcome: (outcome) => {
+    console.log(`${Who} saw outcome ${OUTCOME[outcome]}`);
+  },
+  informTimeout: () => {
+    console.log(`${Who} observed a timeout`);
+  },
+});
 
 await Promise.all([
-    ctcAlice.p.Alice({
-        ...Player('Alice'),
-        wager: stdlib.parseCurrency(5),
-    }),
-    ctcBob.p.Bob({
-        ...Player('Bob'),
-        acceptWager: (amt) => {
-            console.log(`Bob accepts the wager of ${fmt(amt)}.`);
-        },
-    }),
+  ctcAlice.p.Alice({
+    ...Player("Alice"),
+    wager: stdlib.parseCurrency(5),
+    deadline: 10,
+  }),
+  ctcBob.p.Bob({
+    ...Player("Bob"),
+    acceptWager: async (amt) => {
+      /*if (Math.random() <= 0.9) {
+                for (let i = 0; i < 10; i++) {
+                    console.log(`Bob takes his sweet time...`);
+                    await stdlib.wait(1);
+                }
+            } else {*/
+      console.log(`Bob accepts the wager of ${fmt(amt)}.`);
+      //}
+    },
+  }),
 ]);
 
 const afterAlice = await getBalance(accAlice);
